dba918d69e5928cb1c8778043bdb542d84a88edf
Yegor Durko <yegordurko@monke.local>
2020-11-17 21:33:03 +0300

    fix destructuring in geocode

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiZ29nYWJ1cmFrIiwiYSI6ImNrZ2kzOXF6NzAyc3Qyc240MWZjajIyOGUifQ.UB-J8-_jhNEcpgXX2U5wqA&limit=1'
 
-    request({ url, json: true }, (error, { body }) => {
+    request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
         } else if (body.features[0] == undefined) {
